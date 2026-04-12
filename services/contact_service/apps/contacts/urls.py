28cd5676562fe716d5e32a9e38c1478734aac0d1
ehson <ehsonboboev2@gmail.com>
2026-04-12 18:16:38 +0700

    исправил услуги

--- services/contact_service/apps/contacts/urls.py
+++ services/contact_service/apps/contacts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('contacts/add/', views.add_to_contact, name='contact-add'),
     
     # Услуги
-    path('вд/', views.ServiceListView.as_view(), name='service-list'),
+    path('services/', views.ServiceListView.as_view(), name='service-list'),
     path('services/<int:pk>/', views.ServiceDetailView.as_view(), name='service-detail'),
     
     # Сделки
